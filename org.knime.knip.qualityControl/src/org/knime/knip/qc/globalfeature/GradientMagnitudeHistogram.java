079d5af65f57e02eee2737307181f52ce5fd3380
Adrian Nembach <adrian.nembach@gmx.net>
2015-06-11 19:28:25 +0200

    Debugging
    WIP bug not found yet

--- org.knime.knip.qualityControl/src/org/knime/knip/qc/globalfeature/GradientMagnitudeHistogram.java
+++ org.knime.knip.qualityControl/src/org/knime/knip/qc/globalfeature/GradientMagnitudeHistogram.java
@@ -3,23 +3,24 @@ package org.knime.knip.qc.globalfeature;
 import net.imglib2.Cursor;
 import net.imglib2.RandomAccess;
 import net.imglib2.img.Img;
-import net.imglib2.type.numeric.IntegerType;
+import net.imglib2.type.numeric.RealType;
 import net.imglib2.view.ExtendedRandomAccessibleInterval;
 import net.imglib2.view.Views;
 
 public class GradientMagnitudeHistogram {
 
-        public static <T extends IntegerType<T>> double[] calculateGradientMagnitudeHistogram(final Img<T> img, final int min, final int max) {
+        public static <T extends RealType<T>> double[] calculateGradientMagnitudeHistogram(final Img<T> img, final int min, final int max) {
                 final int maxNum = (int) img.firstElement().getMaxValue();
                 final int minNum = (int) img.firstElement().getMinValue();
                 final int rangeNum = maxNum - minNum;
-                final int range = max - min;
+                System.out.println("rangeNum: " + rangeNum);
+                final int range = max - min + 1;
 
                 Cursor<T> center = img.localizingCursor();
-                ExtendedRandomAccessibleInterval<T, Img<T>> outOfBorder = Views.extendMirrorSingle(img);
+                ExtendedRandomAccessibleInterval<T, Img<T>> outOfBounds = Views.extendMirrorSingle(img);
 
-                RandomAccess<T> front = outOfBorder.randomAccess();
-                RandomAccess<T> back = outOfBorder.randomAccess();
+                RandomAccess<T> front = outOfBounds.randomAccess();
+                RandomAccess<T> back = outOfBounds.randomAccess();
 
                 final double[] histogram = new double[range];
 
@@ -29,12 +30,12 @@ public class GradientMagnitudeHistogram {
                         center.localize(pos);
                         double gradientMagnitude = 0;
                         for (int d = 0; d < img.numDimensions(); d++) {
-                                long[] posFront = pos;
-                                posFront[d]++;
-                                long[] posBack = pos;
-                                posBack[d]--;
-                                front.setPosition(posFront);
-                                back.setPosition(posBack);
+                                front.setPosition(pos);
+                                front.fwd(d);
+                                back.setPosition(pos);
+                                back.bck(d);
+                                double f = front.get().getRealDouble();
+                                double b = back.get().getRealDouble();
                                 double g = (front.get().getRealDouble() - back.get().getRealDouble()) * 0.5;
                                 gradientMagnitude += g * g;
                         }
@@ -47,8 +48,14 @@ public class GradientMagnitudeHistogram {
 
                 }
 
+                final long[] dimensions = new long[img.numDimensions()];
+                img.dimensions(dimensions);
+                long numPixels = 1;
+                for (long d : dimensions)
+                        numPixels *= d;
+
                 for (int i = 0; i < histogram.length; i++) {
-                        histogram[i] = histogram[i] / rangeNum;
+                        histogram[i] = histogram[i] / numPixels;
                 }
 
                 return histogram;
