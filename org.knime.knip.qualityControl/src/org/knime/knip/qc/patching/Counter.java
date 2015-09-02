d84a7dcd4cfccf351a0dab851e30c928027e4d34
Adrian Nembach <adrian.nembach@gmx.net>
2015-09-02 12:42:15 +0200

    code review counter
    Introduction of exception if counter is full.

--- org.knime.knip.qualityControl/src/org/knime/knip/qc/patching/Counter.java
+++ org.knime.knip.qualityControl/src/org/knime/knip/qc/patching/Counter.java
@@ -1,44 +1,78 @@
 package org.knime.knip.qc.patching;
 
+/*
+ * This class represents a counter in which each digit can have a different upper bound.
+ * For example a Counter with upper bounds 2 and 3 counts 00, 10, 01, 11, 02, 12.
+ */
 public class Counter {
-        private int[] counter;
-        private int[] boundary;
-        private int curDigit;
+        private int[] m_counter;
+        private int[] m_boundaries;
+        private int m_curDigit;
+        private boolean m_isFull;
 
-        public Counter(int[] boundary) {
-                this.boundary = boundary;
-                this.counter = new int[boundary.length];
-                this.curDigit = 0;
+        public Counter(int[] boundaries) {
+                m_boundaries = boundaries;
+                m_counter = new int[boundaries.length];
+                m_curDigit = 0;
         }
 
         /*
          * Resets counter
          */
         public void reset() {
-                this.counter = new int[boundary.length];
-                this.curDigit = 0;
+                m_counter = new int[m_boundaries.length];
+                m_curDigit = 0;
+                m_isFull = false;
         }
 
         /*
          * Increments counter
          */
-        public void increment() {
-                if (curDigit == counter.length) {
-                        return;
+        public void increment() throws CounterFullException {
+                if (m_isFull) {
+                        throw new CounterFullException();
                 }
-                if (counter[curDigit] == boundary[curDigit] - 1) {
-                        counter[curDigit] = 0;
-                        while (++curDigit < counter.length && counter[curDigit] == boundary[curDigit] - 1)
-                                counter[curDigit] = 0;
-                        if (curDigit == counter.length) {
-                                return;
-                        }
-                        counter[curDigit]++;
-                        curDigit = 0;
+                // if current digit reached its boundary
+                if (m_counter[m_curDigit] == m_boundaries[m_curDigit] - 1) {
+                        // set current digit to 0
+                        m_counter[m_curDigit] = 0;
+                        // check for each following digit if it reached its limit
+                        while (++m_curDigit < m_counter.length && m_counter[m_curDigit] == m_boundaries[m_curDigit] - 1)
+                                m_counter[m_curDigit] = 0;
+                        // increment current digit
+                        m_counter[m_curDigit]++;
+                        // set current digit to first digit
+                        m_curDigit = 0;
                 } else {
-                        counter[curDigit]++;
+                        // increment current digit
+                        m_counter[m_curDigit]++;
                 }
 
+                // check if counter is full
+                boolean isFull = true;
+                for (int i = 0; i < m_counter.length; i++) {
+                        if (m_counter[i] < m_boundaries[i] - 1) {
+                                isFull = false;
+                                break;
+                        }
+                }
+                m_isFull = isFull;
+        }
+
+        /*
+         * Returns the number of digits of the counter
+         * Output: number of digits
+         */
+        public int getNumDigits() {
+                return m_counter.length;
+        }
+
+        /*
+         * Returns whether counter is full
+         * Output: boolean that indicates whether counter is full
+         */
+        public boolean isFull() {
+                return m_isFull;
         }
 
         /*
@@ -47,15 +81,15 @@ public class Counter {
          * Output:  value of digit
          */
         public int getDigit(int index) {
-                return this.counter[index];
+                return this.m_counter[index];
         }
 
         /*
          * Prints the current value for each digit
          */
         public void print() {
-                for (int i = 0; i < this.counter.length; i++)
-                        System.out.print(counter[i]);
+                for (int i = 0; i < this.m_counter.length; i++)
+                        System.out.print(m_counter[i]);
         }
 
 }
---
+++ org.knime.knip.qualityControl/src/org/knime/knip/qc/patching/CounterFullException.java
@@ -0,0 +1,27 @@
+package org.knime.knip.qc.patching;
+
+/*
+ * Exception that is thrown if a full Counter is incremented
+ */
+public class CounterFullException extends Exception {
+
+        public CounterFullException() {
+        }
+
+        public CounterFullException(String arg0) {
+                super(arg0);
+        }
+
+        public CounterFullException(Throwable arg0) {
+                super(arg0);
+        }
+
+        public CounterFullException(String arg0, Throwable arg1) {
+                super(arg0, arg1);
+        }
+
+        public CounterFullException(String arg0, Throwable arg1, boolean arg2, boolean arg3) {
+                super(arg0, arg1, arg2, arg3);
+        }
+
+}
